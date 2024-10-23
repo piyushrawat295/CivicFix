3df69024f74305c0a60ad73582588b53ccffe70b
Piyush Rawat <unexpected086@gmail.com>
2024-10-23 21:25:56 +0530

    Db setup

--- server/app.js
+++ server/app.js
@@ -1,6 +1,23 @@
-const express = require('express');
+import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.error("Failed to connect to MongoDB:", err);
+  });
 
 app.use(express.json()); // Middleware to parse JSON
 
